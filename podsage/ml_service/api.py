c2285fe6133cecf7285bdfcc01845095ccc28b18
unknown <aunsine@gmail.com>
2025-06-19 22:27:24 +0200

    fix docker compose build issue

--- podsage/ml_service/api.py
+++ podsage/ml_service/api.py
@@ -1,13 +1,12 @@
-from fastapi import FASTAPI, Request
+from fastapi import FastAPI, Request
 from sentence_transformers import SentenceTransformer
 
-app = FASTAPI()
-model = SentenceTransformer("all-MiniLM-L6_v2")
+app = FastAPI()
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 @app.post("/embed")
 async def embed(request: Request):
     body = await request.json()
-    text = body.get("text","")
+    text = body.get("text", "")
     embedding = model.encode(text).tolist()
     return embedding
-
